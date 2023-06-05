df9982c04b11a3391744470d27122d8c449a59c4
niklitt <nicklitterio@gmail.com>
2023-06-05 11:37:48 -0600

    feature/MergeTwoSortedLists: added java and python solutions, but tests are incomplete

--- src/main/java/org/litt/Main.java
+++ src/main/java/org/litt/Main.java
@@ -1,12 +1,9 @@
 package org.litt;
 
-import com.opencsv.exceptions.CsvException;
-
-import java.io.IOException;
+import org.testng.Assert;
 
 public class Main {
-    public static void main(String[] args) throws IOException, CsvException {
-        System.out.println("The topography of the mountain is: " + ExcelFileHelper.getValuesFromExcelDoc().toString());
-        System.out.println("The final amount of snow that could fall on the mountain is: " + ExcelFileHelper.logic());
+    public static void main(String[] args) {
+        Assert.assertTrue(true);
     }
 }
