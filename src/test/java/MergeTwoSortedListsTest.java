e8baf03f33f4427abccbb4be906421cd3747cb18
niklitt <nicklitterio@gmail.com>
2023-06-09 10:12:44 -0600

    feature/MergeTwoSortedLists: completed simple test case

--- src/test/java/MergeTwoSortedListsTest.java
+++ src/test/java/MergeTwoSortedListsTest.java
@@ -7,8 +7,16 @@ import org.testng.annotations.Test;
 public class MergeTwoSortedListsTest {
 
     @Test
-    public void test(ListNode list1, ListNode list2) {
-        Solution.mergeTwoLists(list1, list2);
-        Assert.assertTrue(true);
+    public void mergeTwoListsEasy() {
+        ListNode list1 = new ListNode(1,new ListNode(2,new ListNode(4))); // 1, 2, 4
+        System.out.println("list1 is: " + list1);
+        ListNode list2 = new ListNode(1,new ListNode(3,new ListNode(4))); // 1, 3, 4
+        System.out.println("list2 is: " + list2);
+        ListNode actual = Solution.mergeTwoLists(list1, list2);
+        System.out.println("actual is: " + actual);
+        ListNode expected = new ListNode(1, new ListNode(1, new ListNode(2,
+                new ListNode(3, new ListNode(4, new ListNode(4)))))); // 1, 1, 2, 3, 4, 4
+        System.out.println("expected is: " + expected);
+        Assert.assertEquals(actual,expected);
     }
 }
