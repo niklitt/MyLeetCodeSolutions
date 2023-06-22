8b1b8ac370492fe4f1be7137837167a559a765b6
niklitt <nicklitterio@gmail.com>
2023-06-22 14:28:26 -0600

    feature/ZigzagConversion: completed solutions, but still contains commented code that could be converted into a LOGGER

--- src/main/java/org/litt/zigzagConversion/Solution.java
+++ src/main/java/org/litt/zigzagConversion/Solution.java
@@ -1,7 +1,6 @@
 package org.litt.zigzagConversion;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
@@ -19,26 +18,34 @@ public class Solution {
         return stringBuilder.toString();
     }
 
-    private static int doMath(int iteration, int numRows, int currRow, List<Boolean> lastIteration) {
+    private static int doMath(int iteration, int numRows, int currRow, List<Boolean> largeLastIteration) {
         if(numRows <= 2) {
             return iteration + numRows;
         } else {
             int totalRows = numRows - 1;
-            if (lastIteration.get(lastIteration.size() - 1) || totalRows == currRow || totalRows - currRow == totalRows) {
-                // Last iteration was a "big" one so just normally add rows
-                int howCloseToBottom = totalRows - currRow;
+            int howCloseToBottom = totalRows - currRow;
+            if (largeLastIteration.get(largeLastIteration.size() - 1)) {
+                // Last iteration was a "big" one so just normally add rows minus short distance
                 System.out.println("how close to bottom: " + howCloseToBottom);
-                lastIteration.add(false);
+                largeLastIteration.add(false);
+                System.out.println("iteration val: " + iteration);
+                System.out.println("total curr val: " + (iteration + (numRows * 2)));
+                System.out.println("iteration only: " + (numRows * 2));
+                System.out.println();
+                return iteration + (totalRows * 2) - (howCloseToBottom * 2);
+            } else if (totalRows == currRow || totalRows - currRow == totalRows) {
+                // Last iteration was a "big" one so just normally add rows minus short distance
+                System.out.println("how close to bottom: " + howCloseToBottom);
+                largeLastIteration.add(false);
                 System.out.println("iteration val: " + iteration);
                 System.out.println("total curr val: " + (iteration + (numRows * 2)));
                 System.out.println("iteration only: " + (numRows * 2));
                 System.out.println();
                 return iteration + (totalRows * 2);
-            } else {
-                // Last iteration was a normal one so perform logic to get next value
-                int howCloseToBottom = totalRows - currRow;
+            }else {
+                // Last iteration was a normal one so perform logic to get short distance value
                 System.out.println("how close to bottom: " + howCloseToBottom);
-                lastIteration.add(true);
+                largeLastIteration.add(true);
                 System.out.println("iteration val: " + iteration);
                 System.out.println("total curr val: " + (iteration + (numRows * 2)));
                 System.out.println("iteration only: " + (numRows * 2));
