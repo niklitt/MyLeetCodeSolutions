bb168884dfa6f60c7bcf8ddf346b0233d4be8944
niklitt <nicklitterio@gmail.com>
2023-06-24 16:49:03 -0600

    feature/RomanToInteger: added additional test cases

--- src/test/java/RomanToIntegerTest.java
+++ src/test/java/RomanToIntegerTest.java
@@ -15,4 +15,48 @@ public class RomanToIntegerTest {
         System.out.println("expected is: " + expected);
         Assert.assertEquals(actual, expected);
     }
+
+    @Test
+    public void RomanToIntegerIII() {
+        String romanValue = "III";
+        System.out.println("romanValue is: " + romanValue);
+        int actual = Solution.romanToInt(romanValue);
+        System.out.println("actual is: " + actual);
+        int expected = 3;
+        System.out.println("expected is: " + expected);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void RomanToIntegerMCMXCIV() {
+        String romanValue = "MCMXCIV";
+        System.out.println("romanValue is: " + romanValue);
+        int actual = Solution.romanToInt(romanValue);
+        System.out.println("actual is: " + actual);
+        int expected = 1994;
+        System.out.println("expected is: " + expected);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void RomanToIntegerXLIX() {
+        String romanValue = "XLIX";
+        System.out.println("romanValue is: " + romanValue);
+        int actual = Solution.romanToInt(romanValue);
+        System.out.println("actual is: " + actual);
+        int expected = 49;
+        System.out.println("expected is: " + expected);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void RomanToIntegerLI() {
+        String romanValue = "LI";
+        System.out.println("romanValue is: " + romanValue);
+        int actual = Solution.romanToInt(romanValue);
+        System.out.println("actual is: " + actual);
+        int expected = 51;
+        System.out.println("expected is: " + expected);
+        Assert.assertEquals(actual, expected);
+    }
 }
