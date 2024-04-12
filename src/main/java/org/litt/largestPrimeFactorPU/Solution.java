3a1863ae14646f9e08416f571a43a3b5a04704fa
niklitt <nicklitterio@gmail.com>
2024-04-12 14:41:44 -0600

    completed largest prime factor problem

--- src/main/java/org/litt/largestPrimeFactorPU/Solution.java
+++ src/main/java/org/litt/largestPrimeFactorPU/Solution.java
@@ -1,2 +1,27 @@
-package org.litt.largestPrimeFactorPU;public class Solution {
+package org.litt.largestPrimeFactorPU;
+
+public class Solution {
+    public static long calculate(long input) {
+        long result = 0;
+
+        for(int i = 2; i < Math.sqrt(input); i++) {
+            if(input % i == 0) {
+                result = i;
+                System.out.println("The new max prime is: " + result);
+                input = input / i;
+                System.out.println("The new input is: " + input);
+            }
+        }
+
+        if(input > result) {
+            result = input;
+            System.out.println("The new max prime is: " + result);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("The solution is: " + calculate(600851475143L));
+    }
 }
