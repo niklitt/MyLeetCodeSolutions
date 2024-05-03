c8fc07bdaba645fafc492335b3242f357e4afb56
niklitt <nicklitterio@gmail.com>
2024-05-02 19:17:00 -0600

    projecteuler: working on solution for powerDigitSum problem

---
+++ src/main/java/org/litt/powerDigitSumPU/Solution.java
@@ -0,0 +1,25 @@
+package org.litt.powerDigitSumPU;
+
+public class Solution {
+
+    public static int calculate(String powerInput) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(convertStringtoValue(powerInput));
+
+        //TODO: loop through chars and add them
+        stringBuilder.charAt(0);
+
+        return 0;
+    }
+
+    private static long convertStringtoValue(String valueToConvert) {
+
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("The solution for 2^1000 is: " + calculate("2^1000"));
+    }
+
+}
