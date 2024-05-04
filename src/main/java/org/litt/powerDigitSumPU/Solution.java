9ad3df687d2245577bb7e26db26c5738d581d743
niklitt <nicklitterio@gmail.com>
2024-05-03 18:47:41 -0600

    projecteuler: completed powerDigitSum problem.

--- src/main/java/org/litt/powerDigitSumPU/Solution.java
+++ src/main/java/org/litt/powerDigitSumPU/Solution.java
@@ -1,21 +1,42 @@
 package org.litt.powerDigitSumPU;
 
+import java.math.BigInteger;
+
 public class Solution {
 
     public static int calculate(String powerInput) {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(convertStringtoValue(powerInput));
+        String stringValue = String.valueOf(convertStringtoValue(powerInput));
+        int result = 0;
 
         //TODO: loop through chars and add them
-        stringBuilder.charAt(0);
+        for (char val : stringValue.toCharArray()) {
+            result += Character.getNumericValue(val);
+        }
 
-        return 0;
+        return result;
     }
 
-    private static long convertStringtoValue(String valueToConvert) {
+    private static BigInteger convertStringtoValue(String valueToConvert) {
+        char[] valueChars = valueToConvert.toCharArray();
+        BigInteger prePower = new BigInteger("0");
+        int postPower = 0;
+        boolean hitCarrot = false;
 
+        for (char valueChar : valueChars) {
+            if (!hitCarrot && valueChar != '^') {
+                prePower = prePower.multiply(BigInteger.valueOf(10));
+                prePower = prePower.add(BigInteger.valueOf(Character.getNumericValue(valueChar)));
+            } else if (valueChar == '^') {
+                hitCarrot = true;
+            } else {
+                postPower = (postPower * 10) + Character.getNumericValue(valueChar);
+            }
+        }
 
-        return 0;
+        System.out.println("The prePower value is: " + prePower + " and the postPower value is: " + postPower);
+        System.out.println("The result of the Math.pow is: " + prePower.pow(postPower));
+
+        return prePower.pow(postPower);
     }
 
     public static void main(String[] args) {
