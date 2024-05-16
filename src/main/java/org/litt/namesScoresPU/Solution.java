342435e49f760406eeb6e75aeb3288ee74ad0421
niklitt <nicklitterio@gmail.com>
2024-05-16 09:47:49 -0600

    projecteuler: initialized and planned namesScores problem. will need to finish another time

---
+++ src/main/java/org/litt/namesScoresPU/Solution.java
@@ -0,0 +1,13 @@
+package org.litt.namesScoresPU;
+
+/**
+ * Approach:
+ * First read in file
+ * Parse input to get each name and add to List
+ * Learn ASCII values (decide how to approach values of each letter, they are all in caps) ****
+ * Use Collections.sort() or create another sorting method for fun
+ * Put each name into a char array and calculate (value * position) add that to total sum
+ * Return sum
+ */
+public class Solution {
+}
