cd6394a9b1259b92ab7ae9c2a2dac9775204fe07
niklitt <nicklitterio@gmail.com>
2024-05-24 09:45:47 -0600

    projecteuler: completed self powers problem

--- src/main/java/org/litt/selfPowersPU/Solution.java
+++ src/main/java/org/litt/selfPowersPU/Solution.java
@@ -4,17 +4,29 @@ import java.math.BigInteger;
 
 public class Solution {
 
-    public static double calculate(int lastNumber) {
-        double sum = 0;//BigInteger.ZERO;
+    public static String calculate(int lastNumber, int numDigits) {
+        BigInteger sum = BigInteger.ZERO;
 
         for (int i = 1; i <= lastNumber; i++) {
-            sum = Math.pow(i, i);
+            sum = sum.add(BigInteger.valueOf(i).pow(i));
         }
-        return sum;
+
+        char[] chars;
+        chars = sum.toString().toCharArray();
+        char[] lastX = new char[10];
+        int counter = 0;
+        for (int i = chars.length - 1; i > chars.length - numDigits - 1; i--) {
+            lastX[counter] = chars[i];
+            counter++;
+        }
+
+        StringBuilder sb = new StringBuilder(new String(lastX));
+
+        return sb.reverse().toString();
     }
 
     public static void main(String[] args) {
-        System.out.println(calculate(1000));
+        System.out.println(calculate(1000, 10));
     }
 
 }
