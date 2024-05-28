cc02d202e368ca6330a196ebab858108bcb8d6fc
niklitt <nicklitterio@gmail.com>
2024-05-28 10:16:59 -0600

    projecteuler: initialized and started solution for double base palindrome problem

---
+++ src/main/java/org/litt/doubleBasePalindromePU/Solution.java
@@ -0,0 +1,19 @@
+package org.litt.doubleBasePalindromePU;
+
+public class Solution {
+
+    public static int calculate(int cap) {
+        int sum = 0;
+
+        for(int i = 0; i < cap; i++) {
+
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("The sum of all nums under 1000000 who are base 10 and base 2 palindromes is: " + calculate(1000000));
+    }
+
+}
